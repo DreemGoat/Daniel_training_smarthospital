e91d24fd02ff5d5ee47a7d90ed0d39d761557080
DreemGoat <91465902+DreemGoat@users.noreply.github.com>
2026-06-03 10:44:02 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -1,1 +1,13 @@
-print("☆*: .｡. o(≧▽≦)o .｡.:*☆")
+import streamlit as st
+import pandas as pd
+import numpy as np
+import os
+import pickle
+
+st.set_page_config(page_title = "Smart Hospital Patient Navigator" page_icon = "🏥", layout = "wide")
+
+st.markdown("""
+<style>
+@import url ('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
+
+html, body, [class*="css"] {font-family:'Inter', sans-serif;}
