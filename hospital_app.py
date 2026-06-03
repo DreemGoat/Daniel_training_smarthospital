7dfbd4ea11b14f341055f4394de4439cd6b53f3d
DreemGoat <91465902+DreemGoat@users.noreply.github.com>
2026-06-03 10:50:38 +0700

    Update hospital_app.py

--- hospital_app.py
+++ hospital_app.py
@@ -11,3 +11,8 @@ st.markdown("""
 @import url ('https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap');
 
 html, body, [class*="css"] {font-family:'Inter', sans-serif;}
+#MainMenu {visibility:hidden;}
+header[data-testod="stHeader"]{display:none'}.stDeployButton{display:none;}.block-container{padding-top:0 !important;padding-bottom: 2 rem !important; max-widthL 1100px !importajt; }
+div[data-testid="stForm"] {border: none; padding: 0;}
+</style>
+""")
